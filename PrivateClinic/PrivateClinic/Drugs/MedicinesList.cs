8faf43b486b6edcac576430f232aede0fe892020
Vladyslav Lavrov <vladyslav.lavrov@nure.ua>
2021-09-16 11:43:49 +0300

    Add files via upload (#1)

---
+++ PrivateClinic/PrivateClinic/Drugs/MedicinesList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrivateClinic.Drugs
+{
+    public class MedicinesList
+    {
+        public List<Medicine> ListMedicines;
+        public List<string> ListSuppliers;
+
+        public MedicinesList()
+        {
+            
+        }
+
+        public void AddMedicine(Medicine _medicine)
+        {
+            ListMedicines.Add(_medicine);
+            ListSuppliers.Add(_medicine.Fabricator);
+        }
+        
+
+
+    }
+}
